476bace361ef999ec7fcd732e477ca580da83e45
Omar-Arja <omaralarja17@gmail.com>
2023-07-30 13:45:40 +0300

    added login page

--- ecommerce-frontend/js/script.js
+++ ecommerce-frontend/js/script.js
@@ -24,10 +24,46 @@ pages.page_products = () => {
 
 }
 
-// Login page
+// login page
 pages.page_index = () => {
-    
+    const login_email = document.querySelector('#login-email')
+    const login_password = document.querySelector('#login-password')
+    const login_btn = document.querySelector('#login-btn')
+
+    login_btn.addEventListener('click', () => {
+        if (login_email.value != '' && login_password.value != '') {
+            login_btn.innerHTML = 'Loading...'
+            const data = new FormData()
+            data.append('email', login_email.value)
+            data.append('password', login_password.value)
+            
+        }
+    })
 }
+
+// signup page
+pages.page_signup = () => {
+    const signup_name = document.querySelector('#signup-name')
+    const signup_email = document.querySelector('#signup-email')
+    const signup_password = document.querySelector('#signup-password')
+    const signup_confirm_password = document.querySelector('#signup-confirm-password')
+    const signup_btn = document.querySelector('#signup-btn')
+
+    signup_btn.addEventListener('click', () => {
+        if (signup_name.value != '' && signup_email.value != '' && signup_password.value != '' && signup_confirm_password.value != '' && signup_password.value == signup_confirm_password.value) {
+            signup_btn.innerHTML = 'Loading...'
+            const data = new FormData()
+            data.append('name', signup_name.value)
+            data.append('email', signup_email.value)
+            data.append('password', signup_password.value)
+
+        } else {
+            signup_btn.innerHTML = 'Sign Up'
+        }
+
+    })
+}
+            
 
 // navbar
 pages.navbar = () => {
@@ -47,9 +83,13 @@ pages.clickedLink = () => {
             let id = item.id
             pages.activeLink(id)
             clicked_page = id.split('-')[1]
+            console.log(clicked_page)
             eval(`pages.page_${clicked_page}()`)
         })
     })
+}
+pages.page_home = () => {
+    pages.page_dashboard()
 }
 
 // add active class to clicked link
