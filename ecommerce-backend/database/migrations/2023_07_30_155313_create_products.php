e4165cad091667760195d5bc38a7f2a47bf3dbde
Omar-Arja <omaralarja17@gmail.com>
2023-07-30 19:22:43 +0300

    created migrations for the tables in the database

--- ecommerce-backend/database/migrations/2023_07_30_155313_create_products.php
+++ ecommerce-backend/database/migrations/2023_07_30_155313_create_products.php
@@ -11,15 +11,22 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('description');
+            $table->integer('price');
+            $table->string('image_url');
+            $table->integer('category_id');
             $table->timestamps();
         });
+
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
     }
 
     /**
@@ -27,6 +34,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
