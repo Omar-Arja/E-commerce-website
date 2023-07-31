b155412f3d239f08d9234f5f37693d830cc2c203
Omar-Arja <omaralarja17@gmail.com>
2023-07-31 03:29:23 +0300

    created product model and added functions

---
+++ ecommerce-backend/app/Http/Controllers/ProductController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){
+        $products = Product::all();
+        return response()->json([
+            'status' => 'success',
+            'products' => $products,
+        ]);
+    }
+
+    public function show($id){
+        $product = Product::find($id);
+        return response()->json([
+            'status' => 'success',
+            'product' => $product,
+        ]);
+    }
+
+}
--- ecommerce-backend/routes/api.php
+++ ecommerce-backend/routes/api.php
@@ -24,3 +24,16 @@ Route::prefix('/auth')->group(function(){
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
 });
+
+Route::prefix('/admin')->group(function(){
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::get('/products/{id}', [ProductController::class, 'show']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+});
+
+Route::prefix('/products')->group(function(){
+    Route::get('/all-products', [ProductController::class, 'index']);
+    Route::get('/product/{id}', [ProductController::class, 'show']);
+});
